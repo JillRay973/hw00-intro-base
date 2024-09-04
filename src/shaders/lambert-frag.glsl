2146c1fcae6454087c0d1c9ff013a3c2d02bdea1
JillRay973 <143532365+JillRay973@users.noreply.github.com>
2024-09-04 16:08:04 -0400

    lambert-frag.glsl

--- src/shaders/lambert-frag.glsl
+++ src/shaders/lambert-frag.glsl
@@ -1,43 +1,137 @@
 #version 300 es
 
-// This is a fragment shader. If you've opened this file first, please
-// open and read lambert.vert.glsl before reading on.
-// Unlike the vertex shader, the fragment shader actually does compute
-// the shading of geometry. For every pixel in your program's output
-// screen, the fragment shader is run for every bit of geometry that
-// particular pixel overlaps. By implicitly interpolating the position
-// data passed into the fragment shader by the vertex shader, the fragment shader
-// can compute what color to apply to its pixel based on things like vertex
-// position, light position, and vertex color.
 precision highp float;
 
-uniform vec4 u_Color; // The color with which to render this instance of geometry.
-
-// These are the interpolated values out of the rasterizer, so you can't know
-// their specific values without knowing the vertices that contributed to them
 in vec4 fs_Nor;
 in vec4 fs_LightVec;
 in vec4 fs_Col;
+in vec4 fs_Pos;
 
-out vec4 out_Col; // This is the final output color that you will see on your
-                  // screen for the pixel that is currently being processed.
+uniform float u_Time;
+
+out vec4 out_Col;
+
+vec3 random3(vec3 p) {
+    return fract(sin(vec3(dot(p, vec3(185.3, 563.9, 887.2)),
+                          dot(p, vec3(593.1, 591.2, 402.1)),
+                          dot(p, vec3(938.2, 723.4, 768.9))
+                    )) * 58293.492);
+}
+
+vec4 random4(vec4 p) {
+    return fract(sin(vec4(dot(p, vec4(127.1, 311.7, 921.5, 465.8)),
+                          dot(p, vec4(269.5, 183.3, 752.4, 429.1)),
+                          dot(p, vec4(420.6, 631.2, 294.3, 910.8)),
+                          dot(p, vec4(213.7, 808.1, 126.8, 572.0))
+                    )) * 43758.5453);
+}
+
+float surflet(vec4 p, vec4 gridPoint) {
+    vec4 t2 = abs(p - gridPoint);
+    vec4 t = vec4(1.f) - 6.f * pow(t2, vec4(5.f)) + 15.f * pow(t2, vec4(4.f)) - 10.f * pow(t2, vec4(3.f));
+    vec4 gradient = random4(gridPoint) * 2. - vec4(1.);
+    vec4 diff = p - gridPoint;
+    float height = dot(diff, gradient);
+    return height * t.x * t.y * t.z * t.w;
+}
+
+float perlin(vec4 p) {
+	float surfletSum = 0.f;
+
+	for (int dx = 0; dx <= 1; ++dx) {
+		for (int dy = 0; dy <= 1; ++dy) {
+			for (int dz = 0; dz <= 1; ++dz) {
+                for (int dw = 0; dw <= 1; ++dw) {
+				    surfletSum += surflet(p, floor(p) + vec4(dx, dy, dz, dw));
+                }
+			}
+		}
+	}
+
+	return surfletSum;
+}
+
+
+struct WorleyInfo {
+    float dist;
+    vec3 color;
+};
+
+WorleyInfo worley(vec3 uv) {
+    vec3 uvInt = floor(uv);
+    vec3 uvFract = uv - uvInt;
+    float minDist = 1.0f;
+
+    vec3 color;
+    for (int x = -1; x <= 1; ++x) {
+        for (int y = -1; y <= 1; ++y) {
+            for (int z = -1; z <= 1; ++z) {
+                vec3 neighbor = vec3(float(x), float(y), float(z));
+                vec3 point = random3(uvInt + neighbor);
+                vec3 diff = neighbor + point - uvFract;
+                float dist = length(diff);
+                if (dist < minDist) {
+                    minDist = dist;
+                    color = random3(point);
+                }
+            }
+        }
+    }
+
+    WorleyInfo worleyInfo;
+    worleyInfo.dist = minDist;
+    worleyInfo.color = color;
+    return worleyInfo;
+}
+
+struct Shader {
+    vec3 diffuseColor;
+    vec3 emissionColor;
+    float emissionStrength;
+};
+
+Shader mixShaders(Shader a, Shader b, float x) {
+    Shader mixShader;
+    mixShader.diffuseColor = mix(a.diffuseColor, b.diffuseColor, x);
+    mixShader.emissionColor = mix(a.emissionColor, b.emissionColor, x);
+    mixShader.emissionStrength = mix(a.emissionStrength, b.emissionStrength, x);
+    return mixShader;
+}
 
 void main()
 {
-    // Material base color (before shading)
-        vec4 diffuseColor = u_Color;
+    float perlin1 = perlin(vec4(fs_Pos.xyz, float(u_Time) / 1000.f));
+    float perlin2 = perlin(vec4(fs_Pos.xyz * 2.f, float(u_Time) / 2000.f));
+    float perlin3 = perlin(vec4(fs_Pos.xyz * 4.f, float(u_Time) / 4000.f));
 
-        // Calculate the diffuse term for Lambert shading
-        float diffuseTerm = dot(normalize(fs_Nor), normalize(fs_LightVec));
-        // Avoid negative lighting values
-        // diffuseTerm = clamp(diffuseTerm, 0, 1);
+    WorleyInfo swirlyWorley = worley(fs_Pos.xyz + perlin1 + perlin2 + perlin3);
 
-        float ambientTerm = 0.2;
+    Shader swirlyShader;
+    swirlyShader.diffuseColor = swirlyWorley.color * fs_Col.rgb;
+    swirlyShader.emissionColor = swirlyWorley.color;
+    swirlyShader.emissionStrength = smoothstep(0.6, 0.1, swirlyWorley.dist) * 0.75;
 
-        float lightIntensity = diffuseTerm + ambientTerm;   //Add a small float value to the color multiplier
-                                                            //to simulate ambient lighting. This ensures that faces that are not
-                                                            //lit by our point light are not completely black.
+    WorleyInfo circlesWorley1 = worley(fs_Pos.xyz * 10. + vec3(float(u_Time) / 1200.f));
+    WorleyInfo circlesWorley2 = worley(vec3(circlesWorley1.dist * 1.5));
 
-        // Compute final shaded color
-        out_Col = vec4(diffuseColor.rgb * lightIntensity, diffuseColor.a);
+    Shader circlesShader;
+    circlesShader.diffuseColor = vec3(circlesWorley2.dist * 2.0);
+    circlesShader.emissionColor = circlesShader.diffuseColor * fs_Col.rgb;
+    circlesShader.emissionStrength = 0.2;
+
+    float mixFactor = (perlin(vec4(fs_Pos.xyz, 0)) + 1.0) / 2.0;
+    mixFactor = smoothstep(0.4, 0.8, mixFactor);
+    mixFactor = smoothstep(0.0, 1.0, mixFactor);
+    Shader mixShader = mixShaders(swirlyShader, circlesShader, mixFactor);
+
+    float diffuseTerm = dot(normalize(fs_Nor), normalize(fs_LightVec));
+    diffuseTerm = clamp(diffuseTerm, 0., 1.);
+
+    float ambientTerm = 0.05;
+    float lightIntensity = diffuseTerm + ambientTerm;
+
+    vec3 lambertianColor = mixShader.diffuseColor.rgb * lightIntensity;
+    vec3 emissionColor = mixShader.emissionColor * mixShader.emissionStrength;
+
+    out_Col = vec4(lambertianColor + emissionColor, 1);
 }
